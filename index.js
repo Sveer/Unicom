acc8e4a548d7b16644f1b27587a6292b1390e6e0
Sveer <sveer.sveer@gmail.com>
2013-10-08 21:19:26 +0400

    first test

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+window.UnicomStatus = {};
+
+$(function() {
+//DevExpress.devices.current('iPhone');
+    document.addEventListener("deviceready", function() { navigator.splashscreen.hide(); });
+
+    UnicomStatus.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: UnicomStatus,
+        
+        defaultLayout: "empty"
+    });
+
+    // enable iOS7 theme
+	
+    var device = DevExpress.devices.current(),
+        iosVersion = DevExpress.devices.iosVersion();
+    if(device.platform === "ios" && iosVersion && iosVersion[0] === 7) {
+        $(".dx-viewport")
+            .removeClass("dx-theme-ios")
+            .addClass("dx-theme-ios7");
+    }
+
+    UnicomStatus.app.router.register(":view", { view: "home" });
+    UnicomStatus.app.navigate();   
+});
---
+++ views/home.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+UnicomStatus.home = function(params) {
+   
+
+    var billTotal = ko.observable();
+
+   
+	var hasStatus = ko.observable(false);
+	var status = ko.observable('');
+    function checkStatus() {
+        status('');
+		hasStatus(false);
+		
+		$.ajax({
+		 crossDomain: true,
+		 contentType: "application/json; charset=utf-8",
+		 url: "http://status.it-arts.com/webservice1.asmx/GetStatus",
+		  data:{id:billTotal},
+		  dataType:'jsonp'
+		//  method:'POST'
+		}).done(function(dta) {
+		  hasStatus(true);
+		  status(dta.status);
+		}).fail(function(){
+			hasStatus(true);
+		  status('Нет соединения с Интернет.');
+		});
+
+
+    }
+
+
+    return {
+        billTotal: billTotal,
+        hasStatus: hasStatus,
+        status: status,
+
+        checkStatus: checkStatus
+    };
+};
